dadcbcf3b7105176b324671b9c147dfd32599e78
Raja Kumar <rajakumarhjp29@gmail.com>
2026-08-24 19:18:34 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -5,8 +5,10 @@ class CityRepository{
     async createCity({ name }){
         try{
             const city = await City.create({ name });
-            return City;
+            return city;
         } catch(error){
+            console.log("Something went wrong in the repository layer");
+            
             throw{error};
         }
     }
@@ -18,7 +20,33 @@ class CityRepository{
                     id: cityId
                 }
             });
+            return true;
         } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async updateCity(cityId,data){
+        try{
+            const city = await City.update(data, {
+                where: {
+                    cityId
+                }
+            });
+            return city;
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
             throw(error);
         }
     }
