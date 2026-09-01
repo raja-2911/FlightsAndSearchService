549e99cac2ec0b0c4bf631b791b10a3af5c696a6
Raja Kumar <rajakumarhjp29@gmail.com>
2026-09-01 20:00:48 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () =>{
     // create the express object
     const app = express();
+    app.get("/",(req,res)=>{
+            res.send("hello");
+    });
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api',ApiRoutes);
     
     app.listen(PORT, ()=>{
         console.log(`Server started at ${PORT}`);
